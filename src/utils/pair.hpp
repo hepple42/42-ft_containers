f8f86fd3acd12367847247e9014a8925b1aef5cb
Hannes Epple <93395574+hepple42@users.noreply.github.com>
2023-01-26 12:54:53 +0100

    Added first version of pair.hpp

---
+++ src/utils/pair.hpp
@@ -0,0 +1,118 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pair.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hepple <hepple@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 12:05:57 by hepple            #+#    #+#             */
+/*   Updated: 2023/01/26 12:53:28 by hepple           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FT_PAIR_HPP
+#define FT_PAIR_HPP
+
+
+namespace ft {
+
+
+/* *** P A I R ************************************************************** */
+
+template < typename T1, typename T2 >
+struct pair
+{
+
+/* *** MEMBER TYPES ********************************************************* */
+
+	typedef T1	first_type;
+	typedef T2	second_type;
+
+
+/* *** MEMBER VARIABLES ***************************************************** */
+
+	first_type	first;
+	second_type	second;
+
+
+/* *** MEMBER FUNCTIONS ***************************************************** */
+
+/* *** Constructor ********************************************************** */
+
+	pair() : first(), second() { }
+
+	template < typename U, typename V>
+	pair (pair<U, V> const &src) : first(src.first), second(src.second) { }
+
+	pair (first_type const &a, second_type const &b) : first(a) , second(b) { }
+
+/* *** Destructor *********************************************************** */
+
+	~pair() { }
+
+/* *** Assignment *********************************************************** */
+
+	pair &operator=(pair const &src)
+	{
+		first = src.first;
+		second = src.second;
+		return *this;
+	}
+
+};
+
+
+/* *** NON-MEMBER FUNCTION OVERLOADS **************************************** */
+
+/* *** Relational Operators ************************************************* */
+
+template < typename T1, typename T2 >
+bool operator==(pair<T1, T2> const &lhs, pair<T1, T2> const &rhs)
+{
+	return (lhs.first == rhs.first && lhs.second == rhs.second);
+}
+
+template < typename T1, typename T2 >
+bool operator!=(pair<T1, T2> const &lhs, pair<T1, T2> const &rhs)
+{
+	return !(lhs == rhs);
+}
+
+template < typename T1, typename T2 >
+bool operator<(pair<T1, T2> const &lhs, pair<T1, T2> const &rhs)
+{
+	return (lhs.first < rhs.first || (!(rhs.first < lhs.first) && lhs.second < rhs.second));
+}
+
+template < typename T1, typename T2 >
+bool operator<=(pair<T1, T2> const &lhs, pair<T1, T2> const &rhs)
+{
+	return !(rhs < lhs);
+}
+
+template < typename T1, typename T2 >
+bool operator>(pair<T1, T2> const &lhs, pair<T1, T2> const &rhs)
+{
+	return (rhs < lhs);
+}
+
+template < typename T1, typename T2 >
+bool operator>=(pair<T1, T2> const &lhs, pair<T1, T2> const &rhs)
+{
+	return !(lhs < rhs);
+}
+
+/* *** Make Pair ************************************************************ */
+
+template < typename T1, typename T2 >
+pair<T1, T2> make_pair(T1 a, T2 b)
+{
+	return pair<T1, T2>(a, b);
+}
+
+
+} // namespace ft
+
+
+#endif // FT_PAIR_HPP
