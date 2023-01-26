7bdecf5ee0047b7b84166afe47ef96aa436558c5
Hannes Epple <93395574+hepple42@users.noreply.github.com>
2023-01-26 14:32:51 +0100

    Added first version of algorithm.hpp with equal() and lexicographical_compare().

---
+++ include/algorithm.hpp
@@ -0,0 +1,86 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   algorithm.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hepple <hepple@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/26 13:33:09 by hepple            #+#    #+#             */
+/*   Updated: 2023/01/26 14:29:45 by hepple           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef FT_ALGORITHM_HPP
+#define FT_ALGORITHM_HPP
+
+
+namespace ft {
+
+
+/* *** EQUAL **************************************************************** */
+
+template < typename InputIterator1, typename InputIterator2 >
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+{
+	while (first1 != last1)
+	{
+		if (!(*first1 == *first2))
+			return false;
+		++first1;
+		++first2;
+	}
+	return true;
+}
+
+template < typename InputIterator1, typename InputIterator2 , typename BinaryPredicate >
+bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate pred)
+{
+	while (first1 != last1)
+	{
+		if (!pred(*first1, *first2))
+			return false;
+		++first1;
+		++first2;
+	}
+	return true;
+}
+
+
+/* *** LEXICOGRAPHICAL_COMPARE ********************************************** */
+
+template < typename InputIterator1, typename InputIterator2 >
+bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+{
+	while (first1 != last1 && first2 != last2)
+	{
+		if (*first1 < *first2)
+			return true;
+		if (*first2 < *first1)
+			return false;
+		++first1;
+		++first2;
+	}
+	return (first1 == last1 && first2 != last2);
+}
+
+template < typename InputIterator1, typename InputIterator2, typename Compare >
+bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+{
+	while (first1 != last1 && first2 != last2)
+	{
+		if (comp(*first1, *first2))
+			return true;
+		if (comp(*first2, *first1))
+			return false;
+		++first1;
+		++first2;
+	}
+	return (first1 == last1 && first2 != last2);
+}
+
+
+} // namespace ft
+
+
+#endif // FT_ALGORITHM_HPP
