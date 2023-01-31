039afc4f6110450e3e3e30a0b9ac5ef1cfb1f6b8
Hannes Epple <93395574+hepple42@users.noreply.github.com>
2023-01-31 14:43:32 +0100

    Added distance() function.

--- include/iterator.hpp
+++ include/iterator.hpp
@@ -6,7 +6,7 @@
 /*   By: hepple <hepple@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/23 11:16:05 by hepple            #+#    #+#             */
-/*   Updated: 2023/01/30 12:04:08 by hepple           ###   ########.fr       */
+/*   Updated: 2023/01/31 14:42:19 by hepple           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,6 +91,214 @@ template < typename Iterator >
 typename iterator_traits<Iterator>::iterator_category iterator_category(Iterator const &)
 {
 	return typename iterator_traits<Iterator>::iterator_category();
+}
+
+
+/* *** DISTANCE ************************************************************* */
+
+template < typename InputIter >
+typename ft::iterator_traits<InputIter>::difference_type dist(InputIter first, InputIter last, input_iterator_tag)
+{
+	typename ft::iterator_traits<InputIter>::difference_type diff(0);
+	while (first != last)
+	{
+		++diff;
+		++first;
+	}
+	return diff;
+}
+
+template < typename RndIter >
+typename ft::iterator_traits<RndIter>::difference_type dist(RndIter first, RndIter last, random_access_iterator_tag)
+{
+	return last - first;
+}
+
+template < typename InputIter >
+typename ft::iterator_traits<InputIter>::difference_type distance(InputIter first, InputIter last)
+{
+	return dist(first, last, iterator_category(first));
+}
+
+
+/* *** N O R M A L _ I T E R A T O R **************************************** */
+
+template < typename Iterator >
+class normal_iterator
+{
+	
+/* *** MEMBER TYPES ********************************************************* */
+
+	public:
+
+	typedef Iterator												iterator_type;
+	typedef typename iterator_traits<Iterator>::value_type			value_type;
+	typedef typename iterator_traits<Iterator>::difference_type		difference_type;
+	typedef typename iterator_traits<Iterator>::pointer				pointer;
+	typedef typename iterator_traits<Iterator>::reference			reference;
+	typedef typename iterator_traits<Iterator>::iterator_category	iterator_category;
+
+
+/* *** MEMBER VARIABLES ***************************************************** */
+
+	private:
+
+	iterator_type	_current;
+
+
+/* *** MEMBER FUNCTIONS ***************************************************** */
+
+	public:
+
+/* *** Constructor ********************************************************** */
+
+	normal_iterator() : _current() { }
+
+	explicit normal_iterator(iterator_type it) : _current(it) { }
+
+	template < typename Iter >
+	normal_iterator(normal_iterator<Iter> const &n_it) : _current(n_it.base()) { }
+
+/* *** Destructor *********************************************************** */
+
+	~normal_iterator() {}
+
+/* *** Assignment *********************************************************** */
+
+	template < typename Iter >
+	normal_iterator &operator=(normal_iterator<Iter> const &n_it)
+	{
+		_current = n_it.base();
+		return *this;
+	}
+
+/* *** Access *************************************************************** */
+
+	iterator_type base() const
+	{
+		return _current;
+	}
+
+	reference operator[](difference_type n) const
+	{
+		return _current[n];
+	}
+
+	pointer operator->() const
+	{
+		return &(operator*());
+	}
+
+	reference operator*() const
+	{
+		return *_current;
+	}
+
+/* *** Increment / Decrement ************************************************ */
+
+	normal_iterator &operator++()
+	{
+		++_current;
+		return *this;
+	}
+
+	normal_iterator operator++(int)
+	{
+		normal_iterator tmp(*this);
+		++_current;
+		return tmp;
+	}
+
+	normal_iterator &operator+= (difference_type n)
+	{
+		_current += n;
+		return *this;
+	}
+
+	normal_iterator operator+(difference_type n) const
+	{
+		return normal_iterator(_current + n);
+	}
+
+	normal_iterator &operator--()
+	{
+		--_current;
+		return *this;
+	}
+
+	normal_iterator operator--(int)
+	{
+		normal_iterator tmp(*this);
+		--_current;
+		return tmp;
+	}
+
+	normal_iterator &operator-= (difference_type n)
+	{
+		_current -= n;
+		return *this;
+	}
+
+	normal_iterator operator-(difference_type n) const
+	{
+		return normal_iterator(_current - n);
+	}
+
+};
+
+
+/* *** NON-MEMBER FUNCTION OVERLOADS **************************************** */
+
+/* *** Relational Operators ************************************************* */
+
+template < typename IterL, typename IterR >
+bool operator==(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
+{
+	return (lhs.base() == rhs.base());
+}
+
+template < typename IterL, typename IterR >
+bool operator!=(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
+{
+	return (lhs.base() != rhs.base());
+}
+
+template < typename IterL, typename IterR >
+bool operator<(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
+{
+	return (lhs.base() < rhs.base());
+}
+
+template < typename IterL, typename IterR >
+bool operator<=(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
+{
+	return (lhs.base() <= rhs.base());
+}
+
+template < typename IterL, typename IterR >
+bool operator>(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
+{
+	return (lhs.base() > rhs.base());
+}
+
+template < typename IterL, typename IterR >
+bool operator>=(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
+{
+	return (lhs.base() >= rhs.base());
+}
+
+/* *** Increment / Decrement ************************************************ */
+
+template < typename Iter >
+normal_iterator<Iter> operator+(typename normal_iterator<Iter>::difference_type n, normal_iterator<Iter> const &n_it)
+{
+	return normal_iterator<Iter>(n_it.base() + n);
+}
+
+template < typename IterL, typename IterR >
+typename normal_iterator<IterL>::difference_type operator-(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
+{
+	return (lhs.base() - rhs.base());
 }
 
 
@@ -273,187 +481,6 @@ template < typename IterL, typename IterR >
 typename reverse_iterator<IterL>::difference_type operator-(reverse_iterator<IterL> const &lhs, reverse_iterator<IterR> const &rhs)
 {
 	return (rhs.base() - lhs.base());
-}
-
-
-/* *** N O R M A L _ I T E R A T O R **************************************** */
-
-template < typename Iterator >
-class normal_iterator
-{
-	
-/* *** MEMBER TYPES ********************************************************* */
-
-	public:
-
-	typedef Iterator												iterator_type;
-	typedef typename iterator_traits<Iterator>::value_type			value_type;
-	typedef typename iterator_traits<Iterator>::difference_type		difference_type;
-	typedef typename iterator_traits<Iterator>::pointer				pointer;
-	typedef typename iterator_traits<Iterator>::reference			reference;
-	typedef typename iterator_traits<Iterator>::iterator_category	iterator_category;
-
-
-/* *** MEMBER VARIABLES ***************************************************** */
-
-	private:
-
-	iterator_type	_current;
-
-
-/* *** MEMBER FUNCTIONS ***************************************************** */
-
-	public:
-
-/* *** Constructor ********************************************************** */
-
-	normal_iterator() : _current() { }
-
-	explicit normal_iterator(iterator_type it) : _current(it) { }
-
-	template < typename Iter >
-	normal_iterator(normal_iterator<Iter> const &n_it) : _current(n_it.base()) { }
-
-/* *** Destructor *********************************************************** */
-
-	~normal_iterator() {}
-
-/* *** Assignment *********************************************************** */
-
-	template < typename Iter >
-	normal_iterator &operator=(normal_iterator<Iter> const &n_it)
-	{
-		_current = n_it.base();
-		return *this;
-	}
-
-/* *** Access *************************************************************** */
-
-	iterator_type base() const
-	{
-		return _current;
-	}
-
-	reference operator[](difference_type n) const
-	{
-		return _current[n];
-	}
-
-	pointer operator->() const
-	{
-		return &(operator*());
-	}
-
-	reference operator*() const
-	{
-		return *_current;
-	}
-
-/* *** Increment / Decrement ************************************************ */
-
-	normal_iterator &operator++()
-	{
-		++_current;
-		return *this;
-	}
-
-	normal_iterator operator++(int)
-	{
-		normal_iterator tmp(*this);
-		++_current;
-		return tmp;
-	}
-
-	normal_iterator &operator+= (difference_type n)
-	{
-		_current += n;
-		return *this;
-	}
-
-	normal_iterator operator+(difference_type n) const
-	{
-		return normal_iterator(_current + n);
-	}
-
-	normal_iterator &operator--()
-	{
-		--_current;
-		return *this;
-	}
-
-	normal_iterator operator--(int)
-	{
-		normal_iterator tmp(*this);
-		--_current;
-		return tmp;
-	}
-
-	normal_iterator &operator-= (difference_type n)
-	{
-		_current -= n;
-		return *this;
-	}
-
-	normal_iterator operator-(difference_type n) const
-	{
-		return normal_iterator(_current - n);
-	}
-
-};
-
-
-/* *** NON-MEMBER FUNCTION OVERLOADS **************************************** */
-
-/* *** Relational Operators ************************************************* */
-
-template < typename IterL, typename IterR >
-bool operator==(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
-{
-	return (lhs.base() == rhs.base());
-}
-
-template < typename IterL, typename IterR >
-bool operator!=(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
-{
-	return (lhs.base() != rhs.base());
-}
-
-template < typename IterL, typename IterR >
-bool operator<(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
-{
-	return (lhs.base() < rhs.base());
-}
-
-template < typename IterL, typename IterR >
-bool operator<=(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
-{
-	return (lhs.base() <= rhs.base());
-}
-
-template < typename IterL, typename IterR >
-bool operator>(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
-{
-	return (lhs.base() > rhs.base());
-}
-
-template < typename IterL, typename IterR >
-bool operator>=(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
-{
-	return (lhs.base() >= rhs.base());
-}
-
-/* *** Increment / Decrement ************************************************ */
-
-template < typename Iter >
-normal_iterator<Iter> operator+(typename normal_iterator<Iter>::difference_type n, normal_iterator<Iter> const &n_it)
-{
-	return normal_iterator<Iter>(n_it.base() + n);
-}
-
-template < typename IterL, typename IterR >
-typename normal_iterator<IterL>::difference_type operator-(normal_iterator<IterL> const &lhs, normal_iterator<IterR> const &rhs)
-{
-	return (lhs.base() - rhs.base());
 }
 
 
