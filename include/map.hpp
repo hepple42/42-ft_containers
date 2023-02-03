0d9d7fd03f2b402419bb4243157a60593aee7833
Hannes Epple <93395574+hepple42@users.noreply.github.com>
2023-02-03 10:39:02 +0100

    Added prototypes for map member functions.

--- include/map.hpp
+++ include/map.hpp
@@ -6,7 +6,7 @@
 /*   By: hepple <hepple@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 16:41:09 by hepple            #+#    #+#             */
-/*   Updated: 2023/02/02 17:27:32 by hepple           ###   ########.fr       */
+/*   Updated: 2023/02/03 10:32:49 by hepple           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,23 +78,103 @@ class map
 
 /* *** Constructor ********************************************************** */
 
+	explicit map(key_compare const &comp = key_compare(), allocator_type const &alloc = allocator_type());
+
+	template < typename InputIter >
+	map(InputIter first, InputIter last, key_compare const &comp = key_compare(), allocator_type const &alloc = allocator_type());
+
+	map(map const &src);
+
 /* *** Destructor *********************************************************** */
+
+	~map() { }
 
 /* *** Assignment *********************************************************** */
 
+	map &operator=(map const &src);
+
 /* *** Iterators ************************************************************ */
+
+	iterator begin();
+
+	const_iterator begin() const;
+
+	iterator end();
+
+	const_iterator end() const;
+
+	reverse_iterator rbegin();
+
+	const_reverse_iterator rbegin() const;
+
+	reverse_iterator rend();
+
+	const_reverse_iterator rend() const;
 
 /* *** Capacity ************************************************************* */
 
+	bool empty() const;
+
+	size_type size() const;
+
+	size_type max_size() const;
+
 /* *** Element Access ******************************************************* */
+
+	mapped_type &operator[](key_type const &key);
+
+	mapped_type &at(key_type const &key);
+
+	mapped_type const &at(key_type const &key) const;
 
 /* *** Modifiers ************************************************************ */
 
+	pair<iterator, bool> insert(value_type const &val);
+
+	iterator insert(iterator pos, value_type const &val);
+
+	template < typename InputIter >
+	void insert(InputIter first, InputIter last);
+
+	void erase(iterator pos);
+
+	size_type erase(key_type const &key);
+
+	void erase(iterator first, iterator last);
+
+	void swap(map &m);
+
+	void clear();
+
 /* *** Observers ************************************************************ */
+
+	key_compare key_comp() const;
+
+	value_compare value_comp() const;
 
 /* *** Operations *********************************************************** */
 
+	iterator find(key_type const &key);
+
+	const_iterator find(key_type const &key) const;
+
+	size_type count(key_type const &key) const;
+
+	iterator lower_bound(key_type const &key);
+
+	const_iterator lower_bound(key_type const &key) const;
+
+	iterator upper_bound(key_type const &key);
+
+	const_iterator upper_bound(key_type const &key) const;
+
+	pair<const_iterator, const_iterator> equal_range(key_type const &key) const;
+
+	pair<iterator,iterator> equal_range(key_type const &key);
+
 /* *** Allocator ************************************************************ */
+
+	allocator_type get_allocator() const;
 
 };
 
@@ -103,7 +183,28 @@ class map
 
 /* *** Relational Operators ************************************************* */
 
+template < typename Key, typename T, typename Compare, typename Alloc >
+bool operator==(map<Key, T, Compare, Alloc> const &lhs, map<Key, T, Compare, Alloc> const &rhs);
+
+template < typename Key, typename T, typename Compare, typename Alloc >
+bool operator!=(map<Key, T, Compare, Alloc> const &lhs, map<Key, T, Compare, Alloc> const &rhs);
+
+template < typename Key, typename T, typename Compare, typename Alloc >
+bool operator<(map<Key, T, Compare, Alloc> const &lhs, map<Key, T, Compare, Alloc> const &rhs);
+
+template < typename Key, typename T, typename Compare, typename Alloc >
+bool operator<=(map<Key, T, Compare, Alloc> const &lhs, map<Key, T, Compare, Alloc> const &rhs);
+
+template < typename Key, typename T, typename Compare, typename Alloc >
+bool operator>(map<Key, T, Compare, Alloc> const &lhs, map<Key, T, Compare, Alloc> const &rhs);
+
+template < typename Key, typename T, typename Compare, typename Alloc >
+bool operator>=(map<Key, T, Compare, Alloc> const &lhs, map<Key, T, Compare, Alloc> const &rhs);
+
 /* *** Swap ***************************************************************** */
+
+template < typename Key, typename T, typename Compare, typename Alloc >
+void swap(map<Key, T, Compare, Alloc> &lhs, map<Key, T, Compare, Alloc> &rhs);
 
 
 } // namespace ft
