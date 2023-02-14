870ec0d75870b87785590ad21e3d8e968fb3f6f1
Hannes Epple <93395574+hepple42@users.noreply.github.com>
2023-02-14 17:56:03 +0100

    Added very basic red black tree tests.

--- src/main.cpp
+++ src/main.cpp
@@ -6,17 +6,24 @@
 /*   By: hepple <hepple@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 12:14:44 by hepple            #+#    #+#             */
-/*   Updated: 2023/02/03 10:37:20 by hepple           ###   ########.fr       */
+/*   Updated: 2023/02/14 17:03:46 by hepple           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 #include <iostream>
+
+#include <map>
+#include <set>
+#include <stack>
 #include <vector>
 
 #include "../include/map.hpp"
+#include "../include/set.hpp"
 #include "../include/stack.hpp"
 #include "../include/vector.hpp"
+
+#include "../include/red_black_tree.hpp"
 
 int main(void)
 {
@@ -61,6 +68,25 @@ int main(void)
 		std::cout << *it << " ";
 	std::cout << std::endl;
 	}
+
+	// ft::map<int, int> map;
+	// ft::set<int> set;
+
+	{
+		std::less<int> comp;
+		std::allocator<int> alloc;
+		ft::rb_tree<int, std::less<int>, std::allocator<int> > tree(comp, alloc);
+
+		std::cout << tree.size() << std::endl;
+		tree.insert(42);
+		std::cout << tree.size() << std::endl;
+		tree.insert(42);
+		std::cout << tree.size() << std::endl;
+		tree.insert(7);
+		std::cout << tree.size() << std::endl;
+	}
+	
 	std::cout << "Blub." << std::endl;
 	return 0;
+
 }
