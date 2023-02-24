b9cccf6e01ce398e3efb1c5d2b33d4c3bb905b87
Hannes Epple <93395574+hepple42@users.noreply.github.com>
2023-02-24 14:20:52 +0100

    Improved test script.

--- test.sh
+++ test.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-make -s
+# Use Makefile to create two binaries using either the std or the ft containers
+make re -s
 
+# Run both binaries and write the output to the corresponding txt file
 ./std > std.txt
 ./ft > ft.txt
 
+# Run diff command to find potential differences in the output and save results
 diff std.txt ft.txt > result.diff
 diff std.txt ft.txt
